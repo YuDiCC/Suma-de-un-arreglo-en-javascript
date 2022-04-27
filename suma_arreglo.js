9f2f79337011ac1e27b6bb144fc9c98662b87cbf
Yudi Calan Chan <101496814+YuDiCC@users.noreply.github.com>
2022-04-27 04:47:36 -0500

    Add files via upload

---
+++ suma_arreglo.js
@@ -0,0 +1,21 @@
+console.log("Suma de un arreglo")
+console.log(" ")
+let sum=0;
+let lista = [];
+
+function suma (){
+
+    for (i=0; i<=3; i++){
+        lista.push(i);
+        sum = sum + i;
+   }
+    return sum;
+}
+const Sumar = suma();
+
+console.log("El arreglo es", lista)
+console.log(" ")
+console.log("La suma del arreglo es: ", Sumar)
+
+
+
